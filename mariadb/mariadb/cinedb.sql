a93ab2a38ff18ae9a30f20dac35b105cf6a2297f
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-02-02 10:26:19 -0600

    Adding network and volumes

--- mariadb/mariadb/cinedb.sql
+++ mariadb/mariadb/cinedb.sql
@@ -21,8 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `cinev2`
 --
-CREATE DATABASE IF NOT EXISTS `cinev2` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci;
-USE `cinev2`;
+CREATE DATABASE IF NOT EXISTS `cine` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci;
+USE `cine`;
 
 -- --------------------------------------------------------
 
