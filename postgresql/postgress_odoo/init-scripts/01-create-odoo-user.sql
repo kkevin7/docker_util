cd763c2fc1bf8d7ec8fc3234d89b5f280cabf2a3
Kevin Martinez <kkevinmartinez7@gmail.com>
2026-02-26 09:07:15 -0600

    odoo postgress updated

--- postgresql/postgress_odoo/init-scripts/01-create-odoo-user.sql
+++ postgresql/postgress_odoo/init-scripts/01-create-odoo-user.sql
@@ -1,7 +1,15 @@
 -- init-scripts/01-create-odoo-user.sql
--- Create user
-CREATE USER odoo_user WITH PASSWORD 'develop123' CREATEDB;
+
+-- Create superuser
+CREATE USER odoo_user 
+WITH PASSWORD 'develop123'
+SUPERUSER
+CREATEDB
+CREATEROLE
+LOGIN;
+
 -- Create database
 CREATE DATABASE odoo OWNER odoo_user;
+
 -- Grant all privileges on the database to the user
 GRANT ALL PRIVILEGES ON DATABASE odoo TO odoo_user;
